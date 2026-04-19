61a9feca4b911acced57666a671b1ed80bfe847c
davidmzk <sistemmzk@gmail.com>
2026-04-19 13:18:43 -0300

    Agregue perfil David, cambios en la portada, agregue tarjetas en portada y menú

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
-function mostrarMensaje() {
-  document.getElementById("mensaje").innerText = "Gracias por visitar mi perfil 👋";
+//function mostrarMensaje() {
+//document.getElementById("mensaje").innerText = "Gracias por visitar mi perfil 👋";
+//}
+
+// Cambia el modo visual de las tarjetas
+function toggleTarjetas() {
+  document.body.classList.toggle("modo-tarjetas");
+
+  const boton = document.getElementById("btn-luz");
+
+  if (document.body.classList.contains("modo-tarjetas")) {
+    boton.innerText = "Encender la luz";
+  } else {
+    boton.innerText = "Apagar la luz";
+  }
 }
