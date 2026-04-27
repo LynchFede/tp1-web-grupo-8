afb66703b6b02b307703bce56e26004876ae3572
gildamorgante <gigimorgante90@gmail.com>
2026-04-27 18:10:40 -0300

    Deploy html personal (gilda) y funcion de js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@
 //document.getElementById("mensaje").innerText = "Gracias por visitar mi perfil 👋";
 //}
 
+function showMoreGilda() {
+  document.getElementById("contactoGilda").innerHTML = "Contacto: +3415846415";
+}
+
 // Cambia el modo visual de las tarjetas
 function toggleTarjetas() {
   document.body.classList.toggle("modo-tarjetas");
